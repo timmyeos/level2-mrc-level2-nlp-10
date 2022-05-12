7156635e14ef5c4ff3c34b941074f6d03606235a
jimin <eosdbfls@naver.com>
2022-05-12 06:42:18 +0000

    add accelerator

--- code/train_dense.py
+++ code/train_dense.py
@@ -5,7 +5,7 @@ from transformers import (
     TrainingArguments,
 )
 from transformers.models.roberta.modeling_roberta import RobertaModel, RobertaPreTrainedModel
-from datasets import Dataset, concatenate_datasets, load_from_disk
+from datasets import Dataset, DatasetDict, concatenate_datasets, load_from_disk
 from torch.utils.data import (DataLoader, RandomSampler, TensorDataset)
 import torch
 from tqdm import tqdm, trange
@@ -14,6 +14,9 @@ import pickle
 import os
 import numpy as np
 import json
+import pandas as pd
+import pyarrow as pa
+from accelerate import Accelerator
 
 class BertEncoder(BertPreTrainedModel):
   def __init__(self, config):
@@ -55,15 +58,73 @@ def prepare_dataset(retriever, tokenizer, inbatch):
     
     dataset = load_from_disk('../data/train_dataset')
 
-    # train_dataset(train, validation 모두 합쳐 4192개)으로 encoder 훈련
-    dataset = concatenate_datasets(
-        [
-            dataset["train"].flatten_indices(),
-            dataset["validation"].flatten_indices(),
-        ]
-    ) 
+    # # train_dataset(train, validation 모두 합쳐 4192개)으로 encoder 훈련
+    # dataset = concatenate_datasets(
+    #     [
+    #         dataset["train"].flatten_indices(),
+    #         dataset["validation"].flatten_indices(),
+    #     ]
+    # ) 
     print(dataset)
-    # dataset = dataset['validation']
+
+    with open("../data/train_dataset/KorQuAD_v1.0_train.json", "r") as korquad_json:
+        korquad_dataset = json.load(korquad_json)
+
+    korquad___index_level_0__ = []
+    korquad_answers = []
+    korquad_context = []
+    korquad_document_id = []
+    korquad_id = []
+    korquad_question = []
+    korquad_title = []
+
+    for data_ in korquad_dataset['data']:
+        title_ = data_['title']
+        for paragraph_ in data_['paragraphs']:
+            context_ = paragraph_['context']
+            for qas_ in paragraph_['qas']:
+                answers_ = {'answer_start': [qas_['answers'][0]['answer_start']], 'text': [qas_['answers'][0]['text']]}
+                id_ = qas_['id']
+                question_ = qas_['question']
+                korquad___index_level_0__.append(0)
+                korquad_answers.append(answers_)
+                korquad_context.append(context_)
+                korquad_document_id.append(0)
+                korquad_id.append(id_)
+                korquad_question.append(question_)
+                korquad_title.append(title_)    
+
+                if len(korquad___index_level_0__) == 20000:
+                    break
+                
+            if len(korquad___index_level_0__) == 20000:
+                break
+        if len(korquad___index_level_0__) == 20000:
+            break
+
+    train_ = pd.DataFrame({"__index_level_0__": dataset['train']['__index_level_0__'] + korquad___index_level_0__, 
+                           "answers": dataset['train']['answers'] + korquad_answers,
+                           "context": dataset['train']['context'] + korquad_context,
+                           "document_id": dataset['train']['document_id'] + korquad_document_id,
+                           "id": dataset['train']['id'] + korquad_id,
+                           "question": dataset['train']['question'] + korquad_question,
+                           "title": dataset['train']['title'] + korquad_title                 
+                           })
+                           
+    # validation_ = pd.DataFrame({"__index_level_0__": dataset['validation']['__index_level_0__'], 
+    #                        "answers": dataset['validation']['answers'],
+    #                        "context": dataset['validation']['context'],
+    #                        "document_id": dataset['validation']['document_id'],
+    #                        "id": dataset['validation']['id'],
+    #                        "question": dataset['validation']['question'],
+    #                        "title": dataset['validation']['title']                 
+    #                        })
+
+    dataset = DatasetDict({'train':Dataset(pa.Table.from_pandas(train_))})#, 'validation':Dataset(pa.Table.from_pandas(validation_))})
+    print(dataset)
+    dataset = dataset['train']
+    print(dataset)
+
 
     if inbatch == False:
         # sparse embedding -> df : 각 question에 대해 topk passage의 결과를 담은 dataframe
@@ -217,7 +278,7 @@ def train(args, train_dataset, p_model, q_model, num_p_with_negs):
         torch.save(p_model, os.path.join(MODEL_PATH, f"p_encoder{num_epochs}.pt"))
         torch.save(q_model, os.path.join(MODEL_PATH, f"q_encoder{num_epochs}.pt"))
         print('model_saved')
-        train_loss = loss_value / len(epoch_iterator)
+        train_loss = loss_value / len(tepoch)
         train_acc = matches / len(train_dataset)
         print(
             f"Epoch {num_epochs} || training loss {train_loss:4.4} || training accuracy {train_acc:4.2%}"
@@ -232,7 +293,7 @@ def train_inbatch( args, train_dataset, p_model, q_model):
 
     # Dataloader
     train_sampler = RandomSampler(train_dataset)
-    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.per_device_train_batch_size)
+    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.per_device_train_batch_size, drop_last=True)
 
 
     # Optimizer
@@ -246,8 +307,11 @@ def train_inbatch( args, train_dataset, p_model, q_model):
 
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
     t_total = len(train_dataloader) // args.gradient_accumulation_steps * args.num_train_epochs
-    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total)
 
+    accelerator = Accelerator(fp16=args.fp16)
+    p_model, q_model, optimizer, train_dataloader = accelerator.prepare(p_model, q_model, optimizer, train_dataloader)
+
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total)
 
     # Start training!
     global_step = 0
@@ -262,8 +326,8 @@ def train_inbatch( args, train_dataset, p_model, q_model):
 
         with tqdm(train_dataloader, unit="batch") as tepoch:
             for idx,batch in enumerate(tepoch):
-                if torch.cuda.is_available():
-                    batch = tuple(t.cuda() for t in batch)
+                # if torch.cuda.is_available():
+                #     batch = tuple(t.cuda() for t in batch)
                 p_model.train()
                 q_model.train()
 
@@ -289,37 +353,47 @@ def train_inbatch( args, train_dataset, p_model, q_model):
                 sim_scores = torch.matmul(q_outputs, torch.transpose(p_outputs, 0, 1))  #(batch_size, batch_size)
                 sim_scores = F.log_softmax(sim_scores, dim=1)
                 loss = F.nll_loss(sim_scores, targets)
-                if idx%1900 == 0:
-                    print(f'training loss : {loss:.4f}')
 
-                loss.backward()
-                optimizer.zero_grad()
+                accelerator.backward(loss)
                 optimizer.step()
                 scheduler.step()
                 p_model.zero_grad()
                 q_model.zero_grad()
                 global_step += 1
+                
                 torch.cuda.empty_cache()
 
                 del p_inputs, q_inputs
-
+            
+            print(f'training loss : {loss:.4f}')
+        
+        # if num_epochs in [0, 9, 19, 29]: 
+        accelerator.wait_for_everyone()
         MODEL_PATH = "./models"
-        torch.save(p_model, os.path.join(MODEL_PATH, f"p_encoder_inbatch.pt"))
-        torch.save(q_model, os.path.join(MODEL_PATH, f"q_encoder_inbatch.pt"))
+        accelerator.save(accelerator.unwrap_model(p_model).state_dict(), os.path.join(MODEL_PATH, f"p_encoder_inbatch(kor){num_epochs}.pt"))
+        accelerator.save(accelerator.unwrap_model(q_model).state_dict(), os.path.join(MODEL_PATH, f"q_encoder_inbatch(kor){num_epochs}.pt"))
 
     return p_model, q_model
 
 
 def run_dpr(retriever, context, tokenizer, inbatch):
     # dense embedding만 새로 생성하고자 하는 경우
-    q_encoder_name = f"q_encoder.pt"   ### 수정 가능 - 저장된 encoder 중 원하는 모델로
-    p_encoder_name = f"p_encoder.pt"   ### 수정 가능 - 저장된 encoder 중 원하는 모델로
+    q_encoder_name = f"q_encoder_inbatch(kor2만)7.pt"   ### 수정 가능 - 저장된 encoder 중 원하는 모델로
+    p_encoder_name = f"p_encoder_inbatch(kor2만)7.pt"   ### 수정 가능 - 저장된 encoder 중 원하는 모델로
     q_model_path = os.path.join("./models", q_encoder_name)
     p_model_path = os.path.join("./models", p_encoder_name)
 
     if os.path.isfile(q_model_path):
-        q_encoder = torch.load(q_model_path)
-        p_encoder = torch.load(p_model_path)
+        accelerator = Accelerator(fp16=True)
+        q_encoder = accelerator.unwrap_model(BertEncoder.from_pretrained("klue/bert-base"))
+        q_encoder.load_state_dict(torch.load(q_model_path))
+        q_encoder = q_encoder.to('cuda')
+        p_encoder = accelerator.unwrap_model(BertEncoder.from_pretrained("klue/bert-base"))
+        p_encoder.load_state_dict(torch.load(p_model_path))
+        p_encoder = p_encoder.to('cuda')
+        tokenizer = AutoTokenizer.from_pretrained("klue/bert-base") ### reader와 다른 모델 사용 시 주석 제거
+        # q_encoder = torch.load(q_model_path)
+        # p_encoder = torch.load(p_model_path)
     else:
         # load pre-trained model on cuda (if available)
         model_name = "klue/bert-base" ### 수정 가능 - model.args 받아와서 model_args.model_name_or_path 해도 됨
@@ -338,9 +412,9 @@ def run_dpr(retriever, context, tokenizer, inbatch):
             output_dir="dense_retireval",
             evaluation_strategy="epoch",
             learning_rate=2e-5,
-            per_device_train_batch_size=1, ###
-            per_device_eval_batch_size=1, ###
-            num_train_epochs=30, ### 수정 가능
+            per_device_train_batch_size=22, ###
+            per_device_eval_batch_size=22, ###
+            num_train_epochs=10, ### 수정 가능
             weight_decay=0.01,
             fp16 = True
         )
@@ -370,7 +444,7 @@ def run_dpr(retriever, context, tokenizer, inbatch):
     if inbatch == False:
         pickle_name = f"dense_embedding.bin"
     else:
-        pickle_name = f"dense_embedding_inbatch.bin"
+        pickle_name = f"dense_embedding_inbatch(kor).bin"
 
     emd_path = os.path.join(data_path, pickle_name)
     with open(emd_path, "wb") as file:
